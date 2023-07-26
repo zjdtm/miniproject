9e3c3b66382ecfea519a0852716d6e3026a87f02
SYH <syh05247@naver.com>
2023-07-27 02:13:52 +0900

    #4 feat: MemberService save(), duplicateMemberCheck 추가
    * 회원가입시 이메일 중복체크가 필요하다고 판단해서 duplicateMemberCheck 메서드를 추가하였고, 회원을 저장하기 위한 save 메서드도 추가하였다.

--- src/main/java/com/example/miniproject/member/service/MemberService.java
+++ src/main/java/com/example/miniproject/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.example.miniproject.member.service;
 
+import com.example.miniproject.member.domain.Member;
+import com.example.miniproject.member.dto.MemberRequestDto;
 import com.example.miniproject.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
+    public void save(MemberRequestDto memberRequestDto) {
+
+        duplicateMemberCheck(memberRequestDto.getEmail());
+
+        Member member = memberRequestDto.toEntity(passwordEncoder.encode(memberRequestDto.getPassword()));
+
+        memberRepository.save(member);
+    }
+
+    private void duplicateMemberCheck(String email) {
+        if (memberRepository.existsByEmail(email))
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+    }
 
 }
