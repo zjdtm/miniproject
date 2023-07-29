9caf09940071532735ad602d45bbcb090605a763
SYH <syh05247@naver.com>
2023-07-30 00:18:39 +0900

    #7 feat: RefreshToken 엔티티 추가
    * refreshToken 을 DB 에 저장한다고 가정하에 엔티티 를 작성하였다.
    (프론트와 협업 후에 백엔드에서 저장하지 않는다면 삭제 가능성이 있음)
    
    * id, email, refreshToken 를 필드로 가지고 있다.

---
+++ src/main/java/com/example/miniproject/member/domain/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.miniproject.member.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String refreshToken;
+
+    public RefreshToken(String email, String refreshToken) {
+        this.email = email;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+}
