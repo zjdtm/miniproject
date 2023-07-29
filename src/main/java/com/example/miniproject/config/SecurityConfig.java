73bced84fd92eeefe5e5ddb5c362f2a9ff22583b
SYH <syh05247@naver.com>
2023-07-30 00:15:22 +0900

    #7 feat: SecurityConfig 추가
    * csrf 는 우선 disable 로 설정하였으며 Session 기능도 막아둔 상태이다. (이 부분은 추후에 수정 가능성이 있다.)
    
    * /api/register, /api/login 은 요청을 풀어둔 상태이다.
    
    * UsernamePasswordAuthenticationFilter 를 거치기 전 jwtAuthFilter 를 거치도록 적용하였다.
    
    * 비밀번호 암호화를 위해 BCryptPasswordEncoder 를 @Bean 형태로 개발하였다.

---
+++ src/main/java/com/example/miniproject/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.miniproject.config;
+
+import com.example.miniproject.jwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ) // 세션을 사용하지 않도록 STATELESS 를 적용
+                .addFilter(corsFilter)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/register", "/api/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
