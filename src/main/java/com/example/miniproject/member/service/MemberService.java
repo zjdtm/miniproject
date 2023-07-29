4e0eaf54628a1aa66587e32e9d796f70c9344885
SYH <syh05247@naver.com>
2023-07-30 00:05:39 +0900

    #7 feat: MemberService login 메서드 추가
    * LoginMember Dto 를 받아서 회원, 비밀번호를 검증하고 나서 jwtService 를 거쳐서 token 을 생성한다.
    
    * 현재는 accessToken 만 return 하는 식으로 개발하였다. (추후에 refreshToken 도 login 시 같이 전달한다면 리펙터링 해야 할 부분)
    
    * Dto 가 변경됨에 따라 파라미터로 같이 수정하였다.

--- src/main/java/com/example/miniproject/member/service/MemberService.java
+++ src/main/java/com/example/miniproject/member/service/MemberService.java
@@ -1,12 +1,18 @@
 package com.example.miniproject.member.service;
 
+import com.example.miniproject.jwt.service.JwtService;
 import com.example.miniproject.member.domain.Member;
-import com.example.miniproject.member.dto.MemberRequestDto;
 import com.example.miniproject.member.repository.MemberRepository;
+import com.example.miniproject.util.AESUtil;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+
+import static com.example.miniproject.member.dto.MemberRequestDto.*;
 
 @Service
 @Transactional
@@ -14,20 +20,50 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final JwtService jwtService;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public void save(MemberRequestDto memberRequestDto) {
+    // 회원가입
+    public void register(CreateMember memberRequestDto) {
 
         duplicateMemberCheck(memberRequestDto.getEmail());
 
-        Member member = memberRequestDto.toEntity(passwordEncoder.encode(memberRequestDto.getPassword()));
+        Member member = memberRequestDto.toEntity(
+                passwordEncoder.encode(memberRequestDto.getPassword())
+        );
 
         memberRepository.save(member);
     }
 
+    // 로그인
+    public String login(LoginMember memberRequestDto) {
+
+        // 회원이 존재하는지 검증
+        Member member = memberRepository.findByEmail(AESUtil.encrypt(memberRequestDto.getEmail()))
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+
+        // 비밀번호가 일치하는지 검증
+        if(!passwordEncoder.matches(memberRequestDto.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("패스워드를 잘못 입력하였습니다.");
+        }
+
+        String token = jwtService.generateToken(Duration.ofHours(1), member);
+
+        return token;
+
+    }
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+    }
+
+    // 회원 이메일 중복 체크
     private void duplicateMemberCheck(String email) {
         if (memberRepository.existsByEmail(email))
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            throw new IllegalArgumentException("중복된 회원입니다.");
     }
+
+
 
 }
