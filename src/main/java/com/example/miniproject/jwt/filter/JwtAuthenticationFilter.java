bf516507f618d944f874cb317e191987923f20b1
SYH <syh05247@naver.com>
2023-07-30 00:13:21 +0900

    #7 feat: JwtAuthenticationFilter 추가
    * jwtService, PrincipalDetailService 를 필드로 갖는 형태이다.
    
    * OncePerRequestFilter 를 상속하고 있으며 클라이언트에게서 Header 에 Authorization 에 Bearer : token 값을 받으면 검증 단계를 거치고 SecurityContextHolder 에 저장하는 과정을 거치게 개발하였다.

---
+++ src/main/java/com/example/miniproject/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.example.miniproject.jwt.filter;
+
+import com.example.miniproject.jwt.service.JwtService;
+import com.example.miniproject.jwt.service.PrincipalDetailService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final PrincipalDetailService principalDetailService;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authorizationHeader.substring(7);
+        String userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = principalDetailService.loadUserByUsername(userEmail);
+            if (jwtService.validToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        userDetails.getPassword(),
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
