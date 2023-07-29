1a3624d6e7abb790647e82dce94ce50707c4acdf
SYH <syh05247@naver.com>
2023-07-29 23:56:03 +0900

    #7 feat: MemberRepository 에 findByEmail 추가
    * 클라이언트에게서 받은 token 에서 email 추출하고 회원인지 검증하기 위해서 추가하였음

--- src/main/java/com/example/miniproject/member/repository/MemberRepository.java
+++ src/main/java/com/example/miniproject/member/repository/MemberRepository.java
@@ -3,9 +3,12 @@ package com.example.miniproject.member.repository;
 import com.example.miniproject.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 
 }
