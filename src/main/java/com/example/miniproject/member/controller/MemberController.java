80a11ca338fe593f000965353d2ae4522e9c9030
zjdtm <syh05247@naver.com>
2023-07-31 21:46:13 +0900

    #14 fix: MemberController return 변경
    * String -> ResponseCookie 로 변경 header set cookie 설정하기 위해서
    
    * cookie 일단 기본 설정

--- src/main/java/com/example/miniproject/member/controller/MemberController.java
+++ src/main/java/com/example/miniproject/member/controller/MemberController.java
@@ -1,11 +1,21 @@
 package com.example.miniproject.member.controller;
 
+import com.example.miniproject.jwt.service.TokenService;
 import com.example.miniproject.member.dto.MemberRequestDto;
 import com.example.miniproject.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody @Valid MemberRequestDto.CreateMember memberRequestDto) {
@@ -23,12 +34,26 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid MemberRequestDto.LoginMember memberRequestDto) {
+    public ResponseEntity<String> login(
+            HttpServletRequest request,
+            Authentication authentication,
+            @RequestBody @Valid MemberRequestDto.LoginMember memberRequestDto
+    ) {
 
-        String token = memberService.login(memberRequestDto);
+        Map<String, String> tokens = memberService.login(request, memberRequestDto);
 
-        return ResponseEntity.ok().body(token);
+        ResponseCookie responseCookie = ResponseCookie.from(tokens.get("refreshToken"))
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(60)
+                .sameSite("None")           // TODO : why use sameSite ?
+//                .domain("localhost:8080") // TODO : front-end domain
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .body(tokens.get("refreshToken"));
     }
-
 
 }
