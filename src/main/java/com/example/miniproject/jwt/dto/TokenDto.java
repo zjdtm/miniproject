174608176b701977c1cd5b35fb0f1d8490c199e0
zjdtm <syh05247@naver.com>
2023-08-02 16:01:09 +0900

    #24 rename: 폴더 이동 및 클래스 명 변경

--- src/main/java/com/example/miniproject/jwt/dto/TokenDto.java
+++ src/main/java/com/example/miniproject/jwt/dto/TokenDto.java
@@ -1,23 +1,27 @@
-package com.example.miniproject.member.dto;
+package com.example.miniproject.jwt.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 public class TokenDto {
 
     @Getter
     @AllArgsConstructor
-    public static class RequestAccessToken {
+    public static class RequestToken {
 
         private String refreshToken;
 
     }
 
     @Getter
+    @Builder
     @AllArgsConstructor
-    public static class ResponseAccessToken {
+    public static class ResponseToken {
 
         private String accessToken;
+
+        private String refreshToken;
 
     }
 
