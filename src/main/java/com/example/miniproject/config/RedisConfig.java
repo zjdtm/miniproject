422ad173d292e0e179635cecd9687847b060d92e
zjdtm <syh05247@naver.com>
2023-08-02 16:22:37 +0900

    #24 feat: RedisConfig 추가
    * jwt 를 사용하는데 DB 에 저장하는 것 보다는 redis 에 저장이 성능면에서나 좋다고 판단하여서 설정 파일을 추가하였다.

---
+++ src/main/java/com/example/miniproject/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.example.miniproject.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                redisProperties.getHost(),
+                redisProperties.getPort()
+        );
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+
+}
