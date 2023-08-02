afce1fe9ff6f54d06e77ea46e0ce67b66a6079a7
zjdtm <syh05247@naver.com>
2023-08-02 16:18:17 +0900

    #24 comment: jwt 주석 변경
    * jwt 에 이미 토큰이라는 단어가 있기 때문에 토큰 단어 없앰

--- src/main/java/com/example/miniproject/jwt/service/JwtService.java
+++ src/main/java/com/example/miniproject/jwt/service/JwtService.java
@@ -22,7 +22,7 @@ public class JwtService {
     @Value("${jwt.secret_key}")
     private String secretKey;
 
-    // JWT 토큰 생성
+    // JWT 생성
     public String generateToken(Duration expiredAt, Member member) {
         Date now = new Date();
         return makeToken(new Date(now.getTime() + expiredAt.toMillis()), member);
@@ -43,28 +43,28 @@ public class JwtService {
                 .sign(Algorithm.HMAC256(secretKey));
     }
 
-    // JWT 토큰에서 유저네임 추출
+    // JWT 에서 유저네임 추출
     public String extractUsername(String token) {
         return extractAllClaim(token).getSubject();
     }
 
-    // JWT 토큰 유효성 검증
+    // JWT 유효성 검증
     public boolean validToken(String token, UserDetails userDetails) {
         String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !checkTokenExpiration(token));
     }
 
-    // JWT 토큰 만료일자 체크
-    private boolean checkTokenExpiration(String token) {
+    // JWT 만료일자 체크
+    public boolean checkTokenExpiration(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // JWT 토큰에서 만료일자 추출
-    private Date extractExpiration(String token) {
+    // JWT 에서 만료일자 추출
+    public Date extractExpiration(String token) {
         return extractAllClaim(token).getExpiresAt();
     }
 
-    // JWT 토큰으로 모든 Claim 추출
+    // JWT 에서 모든 Claim 추출
     public DecodedJWT extractAllClaim(String token) {
         return JWT.require(Algorithm.HMAC256(secretKey))
                 .build()
