daa349cf9b940a14d408ea19b433dd1ebfd24912
zjdtm <syh05247@naver.com>
2023-08-02 16:37:27 +0900

    #24 refactor: TokenService, TokenApiController 수정
    * cookie 값에 따라 새로운 accessToken 을 body 에 담아서 보내도록 설정하였다.
    
    * redis 를 적용함에 따랄 유효성 검증 및 저장을 수정하였다.

--- src/main/java/com/example/miniproject/jwt/service/TokenService.java
+++ src/main/java/com/example/miniproject/jwt/service/TokenService.java
@@ -1,32 +1,52 @@
 package com.example.miniproject.jwt.service;
 
+import com.example.miniproject.constant.ErrorCode;
+import com.example.miniproject.exception.TokenException;
 import com.example.miniproject.member.domain.Member;
 import com.example.miniproject.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Service
 public class TokenService {
 
     private final JwtService jwtService;
-    private final RefreshTokenService refreshTokenService;
+
+    private final RedisTemplate redisTemplate;
+
     private final PrincipalDetailService principalDetailService;
     private final MemberService memberService;
 
-    public String createNewAccessToken(String refreshToken, Authentication authentication) {
+    public String createNewAccessToken(String refreshTokenId, Authentication authentication) {
 
-        if (!jwtService.validToken(refreshToken, principalDetailService.loadUserByUsername(authentication.getName()))) {
-            throw new IllegalArgumentException("토큰 유효성 검증에 실패하였습니다.");
+        // redis 에서 회원 정보를 이용해서 jwt 가 존재하는지 확인
+        String findRefreshToken = redisTemplate.opsForValue().get(refreshTokenId).toString();
+        if(Objects.isNull(findRefreshToken))
+            throw new TokenException(ErrorCode.TOKEN_NOT_FOUND);
+
+        // refreshToken 유효성 검증
+        if (!jwtService.validToken(findRefreshToken, principalDetailService.loadUserByUsername(authentication.getName()))
+        ) {
+            throw new TokenException(ErrorCode.TOKEN_NOT_MATCH);
         }
 
-        String email = refreshTokenService.findByRefreshToken(refreshToken).getEmail();
+        String email = authentication.getName();
         Member member = memberService.findByEmail(email);
 
-        return jwtService.generateToken(Duration.ofHours(2), member);
+        redisTemplate.opsForValue().set(
+                refreshTokenId, refreshTokenId,
+                jwtService.extractAllClaim(refreshTokenId).getExpiresAt().getTime(),
+                TimeUnit.MILLISECONDS
+        );
+
+        return jwtService.generateToken(Duration.ofHours(1), member);
 
     }
 
--- src/main/java/com/example/miniproject/member/controller/TokenApiController.java
+++ src/main/java/com/example/miniproject/member/controller/TokenApiController.java
@@ -1,29 +1,39 @@
 package com.example.miniproject.member.controller;
 
 import com.example.miniproject.jwt.service.TokenService;
-import com.example.miniproject.member.dto.TokenDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class TokenApiController {
 
     private final TokenService tokenService;
 
-    @PostMapping("/api/token")
-    public ResponseEntity<TokenDto.ResponseAccessToken> createNewAccessToken(
-            @RequestBody TokenDto.RequestAccessToken request,
-            Authentication authentication) {
-        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken(), authentication);
+    @PostMapping("/token")
+    public ResponseEntity<String> createNewAccessToken(
+            HttpServletRequest request, Authentication authentication
+    ) {
+
+        Cookie[] cookies = request.getCookies();
+
+        String refreshTokenId = null;
+        for (Cookie cookie : cookies) {
+            refreshTokenId = cookie.getName();
+        }
+
+        String newAccessToken = tokenService.createNewAccessToken(refreshTokenId, authentication);
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new TokenDto.ResponseAccessToken(newAccessToken));
+                .body(newAccessToken);
     }
 
 }
