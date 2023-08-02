702972e7b4a005b911557bd728aabdce096e4f54
zjdtm <syh05247@naver.com>
2023-08-02 15:55:18 +0900

    #24 feat: TokenException Handler 에 추가
    * GlobalExceptionHandler 에 TokenException 추가하였음

--- src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
@@ -1,17 +1,16 @@
 package com.example.miniproject.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	@ExceptionHandler({MemberException.class, AnnualException.class})
-	public ResponseEntity<?> handlerMemberException(DomainException e) {
-		log.error(e.getMessage());
-		return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
-	}
+    @ExceptionHandler({MemberException.class, AnnualException.class, TokenException.class})
+    public ResponseEntity<?> handlerMemberException(DomainException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+    }
 }
