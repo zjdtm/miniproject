d492953a88d8b103c751fbe42afd83c7d451df78
SeongHyeon Park <seonghye0n@kakao.com>
2023-08-03 17:12:42 +0900

    #52 refactor: 회원 엔티티에 총 연차 수, 사용 연차수, 잔여 연차수 저장하도록 구조 변경 및 기능 추가

--- src/main/java/com/example/miniproject/member/dto/MemberRequestDto.java
+++ src/main/java/com/example/miniproject/member/dto/MemberRequestDto.java
@@ -1,64 +1,69 @@
 package com.example.miniproject.member.dto;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.example.miniproject.constant.Role;
 import com.example.miniproject.member.domain.Member;
+import com.example.miniproject.member.domain.TotalAnnual;
 import com.example.miniproject.util.AESUtil;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 public class MemberRequestDto {
 
-    @Getter
-    @Builder
-    public static class CreateMember {
+	@Getter
+	@Builder
+	public static class CreateMember {
 
-        @NotNull
-        private String name;
+		@NotNull
+		private String name;
 
-        @NotNull
-        @Email
-        private String email;
+		@NotNull
+		@Email
+		private String email;
 
-        @NotNull
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
-        private String password;
+		@NotNull
+		@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
+		private String password;
 
-        @NotNull
-        @DateTimeFormat(pattern = "yyyy-MM-dd")
-        private LocalDate join;
+		@NotNull
+		@DateTimeFormat(pattern = "yyyy-MM-dd")
+		private LocalDate join;
 
-        public Member toEntity(String password) {
+		public Member toEntity(String password, TotalAnnual totalAnnual) {
 
-            return Member.builder()
-                    .name(AESUtil.encrypt(name))
-                    .email(AESUtil.encrypt(email))
-                    .password(password)
-                    .role(Role.USER)
-                    .joinedAt(join.atStartOfDay())
-                    .build();
-        }
+			return Member.builder()
+				.name(AESUtil.encrypt(name))
+				.email(AESUtil.encrypt(email))
+				.password(password)
+				.role(Role.USER)
+				.joinedAt(join.atStartOfDay())
+				.totalAnnual(totalAnnual)
+				.annualUsed(0)
+				.annualRemain(totalAnnual.getAnnualAmount())
+				.build();
+		}
 
-    }
+	}
 
-    @Getter
-    @Builder
-    public static class LoginMember {
+	@Getter
+	@Builder
+	public static class LoginMember {
 
-        @NotNull
-        @Email
-        private String email;
+		@NotNull
+		@Email
+		private String email;
 
-        @NotNull
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
-        private String password;
+		@NotNull
+		@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$")
+		private String password;
 
-    }
-
+	}
 
 }
