9fc6a7811a10b044130951293578631fc5a3ac7b
SYH <syh05247@naver.com>
2023-08-06 21:07:34 +0900

    #63 style: 코드 수정 없이 들여쓰기만 수정됨

--- src/main/java/com/example/miniproject/annual/domain/Annual.java
+++ src/main/java/com/example/miniproject/annual/domain/Annual.java
@@ -1,35 +1,19 @@
 package com.example.miniproject.annual.domain;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import com.example.miniproject.annual.dto.AnnualRequestDto;
 import com.example.miniproject.constant.Category;
 import com.example.miniproject.constant.Reason;
 import com.example.miniproject.constant.Status;
 import com.example.miniproject.member.domain.Member;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -39,43 +23,43 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table(name = "reg")
 public class Annual {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotNull
-	@Enumerated(EnumType.STRING)
-	private Category category;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
-	@NotBlank
-	private String title;
-	@NotNull
-	private LocalDate startedAt;
-	@NotNull
-	private LocalDate lastedAt;
+    @NotBlank
+    private String title;
+    @NotNull
+    private LocalDate startedAt;
+    @NotNull
+    private LocalDate lastedAt;
 
-	@Enumerated(EnumType.STRING)
-	private Reason reason;
+    @Enumerated(EnumType.STRING)
+    private Reason reason;
 
-	@NotNull
-	@Enumerated(EnumType.STRING)
-	private Status status;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
-	@Column(nullable = false)
-	@CreationTimestamp
-	private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-	@UpdateTimestamp
-	private LocalDateTime modifiedAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
 
-	public void updateData(AnnualRequestDto.UpdateDto updateDto) {
-		this.title = updateDto.getTitle();
-		this.startedAt = LocalDate.parse(updateDto.getStartDate());
-		this.lastedAt = LocalDate.parse(updateDto.getEndDate());
-		this.reason = Reason.findByName(updateDto.getReason());
-	}
+    public void updateData(AnnualRequestDto.UpdateDto updateDto) {
+        this.title = updateDto.getTitle();
+        this.startedAt = LocalDate.parse(updateDto.getStartDate());
+        this.lastedAt = LocalDate.parse(updateDto.getEndDate());
+        this.reason = Reason.findByName(updateDto.getReason());
+    }
 }
--- src/main/java/com/example/miniproject/annual/repository/AnnualRepository.java
+++ src/main/java/com/example/miniproject/annual/repository/AnnualRepository.java
@@ -1,13 +1,12 @@
 package com.example.miniproject.annual.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.miniproject.annual.domain.Annual;
 import com.example.miniproject.constant.Category;
 import com.example.miniproject.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AnnualRepository extends JpaRepository<Annual, Long> {
-	List<Annual> findAllByMemberAndCategory(Member member, Category category);
+    List<Annual> findAllByMemberAndCategory(Member member, Category category);
 }
