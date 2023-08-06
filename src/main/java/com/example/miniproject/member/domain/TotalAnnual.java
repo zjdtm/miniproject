3ea0d76815bac9c44c2802b75aee655c0677e582
SYH <syh05247@naver.com>
2023-08-06 21:07:57 +0900

    #63 feat: id 자동 증가 애노테이션 추가

--- src/main/java/com/example/miniproject/member/domain/TotalAnnual.java
+++ src/main/java/com/example/miniproject/member/domain/TotalAnnual.java
@@ -1,8 +1,6 @@
 package com.example.miniproject.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,7 @@ import lombok.Setter;
 @Table(name = "annual")
 public class TotalAnnual {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private int years;
 	private int annualAmount;
