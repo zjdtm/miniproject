e9497cba416c9b59ae44fad8431fb95368757033
SYH <syh05247@naver.com>
2023-08-06 21:02:03 +0900

    #63 refactor: GlobalExceptionHandler 리펙터링
    * json 형태로 프론트에게 보내기 위해서 ErrorResponse 객체를 ResponseEntity 에 파라미터로 넣는 것으로 수정

--- src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
@@ -1,19 +1,20 @@
 package com.example.miniproject.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.security.SignatureException;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({MemberException.class, AnnualException.class, TokenException.class})
+
+    @ExceptionHandler(DomainException.class)
     public ResponseEntity<?> handlerMemberException(DomainException e) {
         log.error(e.getMessage());
-        return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+        ErrorResponse response = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(response.getStatus()));
     }
+
 }
