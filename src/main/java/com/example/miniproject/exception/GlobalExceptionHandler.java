4d96d78c6286edef5bfeac361fbd1ebc862f9bc9
zjdtm <syh05247@naver.com>
2023-08-08 21:33:07 +0900

    #87 feat: exception handler 추가
    * 회원가입시 날짜 형식이 맞지 않으면 처리하는 handler 처리하는 로직을 추가함

--- src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/miniproject/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.example.miniproject.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.format.DateTimeParseException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,6 +23,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(response.getStatus()));
     }
 
+    // MethodArgumentNotValidException 에러 발생시 처리하는 handler
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handlerValidationException(MethodArgumentNotValidException e) {
         ObjectError error = e.getBindingResult().getAllErrors().stream().findFirst().get();
@@ -33,4 +38,18 @@ public class GlobalExceptionHandler {
         );
     }
 
+    // 회원가입시 날짜 형식이 맞지 않으면 처리하는 handler
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<?> handlerDateTimeParseException(
+            HttpMessageNotReadableException e
+    ) {
+
+        String errorMessage = "날짜 형식이 올바르지 않습니다. (yyyy-MM-dd)";
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage);
+
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
 }
