01d216c977b5d218ddf77b564d2944e4e70c2b12
SeongHyeon Park <seonghye0n@kakao.com>
2023-08-08 14:35:29 +0900

    fix: annualHistories 오타 수정

--- src/main/java/com/example/miniproject/annual/dto/AnnualResponseDto.java
+++ src/main/java/com/example/miniproject/annual/dto/AnnualResponseDto.java
@@ -1,109 +1,113 @@
 package com.example.miniproject.annual.dto;
 
-import com.example.miniproject.annual.domain.Annual;
-import com.example.miniproject.member.domain.Member;
-import com.example.miniproject.util.AESUtil;
-import com.example.miniproject.util.DateUtil;
-import lombok.*;
-
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.miniproject.annual.domain.Annual;
+import com.example.miniproject.member.domain.Member;
+import com.example.miniproject.util.AESUtil;
+import com.example.miniproject.util.DateUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 public class AnnualResponseDto {
-    @Getter
-    @Setter
-    public static class MainDto {
+	@Getter
+	@Setter
+	public static class MainDto {
 
-        private String username;
+		private String username;
 
-        private List<AnnualCalendar> annuals;
+		private List<AnnualCalendar> annuals;
 
-        public MainDto(String username, List<Annual> annuals) {
-            this.username = AESUtil.decrypt(username);
-            this.annuals = annuals.stream().map(annual ->
-                    AnnualCalendar.builder()
-                            .id(annual.getId())
-                            .email(AESUtil.decrypt(annual.getMember().getEmail()))
-                            .name(AESUtil.decrypt(annual.getMember().getName()))
-                            .category(annual.getCategory().getName())
-                            .startDate(annual.getStartedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-                            .endDate(annual.getLastedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-                            .reason(annual.getReason() == null ? null : annual.getReason().getName())
-                            .build()
-                    ).collect(Collectors.toList());
-        }
+		public MainDto(String username, List<Annual> annuals) {
+			this.username = AESUtil.decrypt(username);
+			this.annuals = annuals.stream().map(annual ->
+				AnnualCalendar.builder()
+					.id(annual.getId())
+					.email(AESUtil.decrypt(annual.getMember().getEmail()))
+					.name(AESUtil.decrypt(annual.getMember().getName()))
+					.category(annual.getCategory().getName())
+					.startDate(annual.getStartedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+					.endDate(annual.getLastedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+					.reason(annual.getReason() == null ? null : annual.getReason().getName())
+					.build()
+			).collect(Collectors.toList());
+		}
 
-        @Getter
-        @Setter
-        @Builder
-        @AllArgsConstructor
-        private static class AnnualCalendar {
-            private Long id;
-            private String email;
-            private String name;
-            private String category;
-            private String startDate;
-            private String endDate;
-            private String reason;
-        }
-    }
+		@Getter
+		@Setter
+		@Builder
+		@AllArgsConstructor
+		private static class AnnualCalendar {
+			private Long id;
+			private String email;
+			private String name;
+			private String category;
+			private String startDate;
+			private String endDate;
+			private String reason;
+		}
+	}
 
-    @Getter
-    @Setter
-    public static class MyPageDto {
-        private String name;
-        private int annualBalance;
-        private int annualUsed;
-        private int annualRemain;
-        private List<AnnualHistroy> annualHistroies;
-        private List<DutyHistory> dutyHistories;
+	@Getter
+	@Setter
+	public static class MyPageDto {
+		private String name;
+		private int annualBalance;
+		private int annualUsed;
+		private int annualRemain;
+		private List<AnnualHistroy> annualHistories;
+		private List<DutyHistory> dutyHistories;
 
-        public MyPageDto(Member member, List<AnnualHistroy> annualHistroies, List<DutyHistory> dutyHistories) {
-            this.name = AESUtil.decrypt(member.getName());
-            this.annualBalance = member.getTotalAnnual().getAnnualAmount();
-            this.annualUsed = member.getAnnualUsed();
-            this.annualRemain = member.getAnnualRemain();
-            this.annualHistroies = annualHistroies;
-            this.dutyHistories = dutyHistories;
-        }
-    }
+		public MyPageDto(Member member, List<AnnualHistroy> annualHistories, List<DutyHistory> dutyHistories) {
+			this.name = AESUtil.decrypt(member.getName());
+			this.annualBalance = member.getTotalAnnual().getAnnualAmount();
+			this.annualUsed = member.getAnnualUsed();
+			this.annualRemain = member.getAnnualRemain();
+			this.annualHistories = annualHistories;
+			this.dutyHistories = dutyHistories;
+		}
+	}
 
-    @Getter
-    @Setter
-    public static class AnnualHistroy {
-        private Long id;
-        private String title;
-        private String reason;
-        private String startDate;
-        private String endDate;
-        private String status;
+	@Getter
+	@Setter
+	public static class AnnualHistroy {
+		private Long id;
+		private String title;
+		private String reason;
+		private String startDate;
+		private String endDate;
+		private String status;
 
-        public AnnualHistroy(Annual annual) {
-            this.id = annual.getId();
-            this.title = annual.getTitle();
-            this.reason = annual.getReason().getName();
-            this.startDate = DateUtil.toDateFormat(annual.getStartedAt());
-            this.endDate = DateUtil.toDateFormat(annual.getLastedAt());
-            this.status = annual.getStatus().getName();
-        }
-    }
+		public AnnualHistroy(Annual annual) {
+			this.id = annual.getId();
+			this.title = annual.getTitle();
+			this.reason = annual.getReason().getName();
+			this.startDate = DateUtil.toDateFormat(annual.getStartedAt());
+			this.endDate = DateUtil.toDateFormat(annual.getLastedAt());
+			this.status = annual.getStatus().getName();
+		}
+	}
 
-    @Getter
-    @Setter
-    public static class DutyHistory {
-        private Long id;
-        private String title;
-        private String startDate;
-        private String endDate;
-        private String status;
+	@Getter
+	@Setter
+	public static class DutyHistory {
+		private Long id;
+		private String title;
+		private String startDate;
+		private String endDate;
+		private String status;
 
-        public DutyHistory(Annual annual) {
-            this.id = annual.getId();
-            this.title = annual.getTitle();
-            this.startDate = DateUtil.toDateFormat(annual.getStartedAt());
-            this.endDate = DateUtil.toDateFormat(annual.getLastedAt());
-            this.status = annual.getStatus().getName();
-        }
-    }
+		public DutyHistory(Annual annual) {
+			this.id = annual.getId();
+			this.title = annual.getTitle();
+			this.startDate = DateUtil.toDateFormat(annual.getStartedAt());
+			this.endDate = DateUtil.toDateFormat(annual.getLastedAt());
+			this.status = annual.getStatus().getName();
+		}
+	}
 }
