a8e0d5be5d1ac1dd810711c29794fb63f17c6721
zjdtm <syh05247@naver.com>
2023-08-08 15:54:32 +0900

    #79 bug: 쿠키 에러처리
    * 쿠키가 없을 때는 다음 필터로 처리

--- src/main/java/com/example/miniproject/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/miniproject/jwt/filter/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         Cookie[] cookies = request.getCookies();
 
         // 쿠키가 없을 때는 다음 필터로
-        if(cookies != null && cookies.length >0) {
+        if(cookies == null && cookies.length == 0) {
             filterChain.doFilter(request, response);
             return;
         }
